a8914c2ae5352bd0834d834ce5e4004486120788
MclPio <113801014+MclPio@users.noreply.github.com>
2025-06-28 15:45:17 -0400

    Add daily usage for rate limiting addressing issue #1

--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  include OpenRouterUsageTracker::Trackable
 end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_19_233146) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_28_192523) do
+  create_table "open_router_daily_summaries", force: :cascade do |t|
+    t.string "user_type", null: false
+    t.integer "user_id", null: false
+    t.date "day", null: false
+    t.integer "total_tokens", default: 0, null: false
+    t.decimal "cost", precision: 10, scale: 5, default: "0.0", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_type", "user_id", "day"], name: "index_daily_summaries_on_user_and_day", unique: true
+    t.index ["user_type", "user_id"], name: "index_open_router_daily_summaries_on_user"
+  end
+
   create_table "open_router_usage_logs", force: :cascade do |t|
     t.string "model", null: false
     t.integer "prompt_tokens", null: false
@@ -23,8 +35,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_19_233146) do
     t.json "raw_usage_response", default: {}, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index [ "request_id" ], name: "index_open_router_usage_logs_on_request_id", unique: true
-    t.index [ "user_type", "user_id" ], name: "index_open_router_usage_logs_on_user"
+    t.index ["request_id"], name: "index_open_router_usage_logs_on_request_id", unique: true
+    t.index ["user_type", "user_id"], name: "index_open_router_usage_logs_on_user"
   end
 
   create_table "users", force: :cascade do |t|
